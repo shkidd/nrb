53be112c31715766fd2957a156051417dc53aef3
Scott Kidd <scottkidd@Scott-Kidds-MacBook-Pro.local>
2014-01-26 15:36:28 -0500

    script update

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 $(document).ready(function() {
 	
+
 	var width = $(window).width();
 	
-	$('#section2 #wrapper').width( "45%");
-	$('#wrapper h1').css('font-size' , width/1284 * 350 + "%");
-	$('#section2 #wrapper h1').css('margin-top' , "2em");
-	$('#wrapper h1').css('margin-left' , "1.45em");
-	$('#wrapper p').css('font-size' , width/1284 * 100 + "%");
-	$('#section2 #wrapper p').css('margin-top' , "15em");
-	$('#wrapper p').css('margin-left' , "5em");
-	$('#button2').css('font-size' , width/1284 * 100 + "%");
-	$('#button2').css('margin-top' , "30em");
-	$('#button2').css('margin-left' , "5em");	
-	$('#section4 #wrapper').width( "55%");
-	$('#section4 #wrapper h1').css('margin-top' , "1em");
-	$('#section4 #wrapper p').css('margin-top' , "8em");	
-	$('#section4 .button').css('font-size' , width/1284 * 100 + "%");
-	$('#section4 .button').css('margin-top' , "18em");
-	$('#section4 .button').css('margin-left' , "5em");	
+	// $('#section2 #wrapper').width( "45%");
+	// $('#wrapper h1').css('font-size' , width/1284 * 350 + "%");
+	// $('#wrapper h1').css('margin-left' , "1.45em");
+	// $('#wrapper p').css('font-size' , width/1284 * 100 + "%");
+	// $('#wrapper p').css('margin-left' , "5em");
+	// $('#wrapper button').css('font-size' , width/1284 * 100 + "%");
+	// $('#wrapper button').css('margin-left' , "5em");
 	
 
 	var hidden = false; var note_hidden = false; var small = false;
-	
-	if(width < 800){
-		smallVerson();
-	}
 
 	$(window).scroll(function() {  
 		if($(window).scrollTop() > 20 && hidden == false && $('#scrolling-header').is(':hover') == false){
@@ -56,28 +44,19 @@ $(document).ready(function() {
 	
 	$(window).resize(function(){
 		 width = $(window).width();
-		$('#section2 #wrapper').width( "45%");
-		$('#wrapper h1').css('font-size' , width/1284 * 350 + "%");
-		$('#section2 #wrapper h1').css('margin-top' , "2em");
-		$('#wrapper h1').css('margin-left' , "1.45em");
-		$('#wrapper p').css('font-size' , width/1284 * 100 + "%");
-		$('#section2 #wrapper p').css('margin-top' , "15em");
-		$('#wrapper p').css('margin-left' , "5em");	
-		$('#button2').css('font-size' , width/1284 * 100 + "%");
-		$('#button2').css('margin-top' , "19em");
-		$('#button2').css('margin-left' , "5em");
+		if (width > 850) {
+			$('#rightSmall').css('height', width/1284 * 50 + "em");
+		}
 		
-		$('#section4 #wrapper').width( "55%");
-		$('#section4 #wrapper h1').css('margin-top' , "1em");
-		$('#section4 #wrapper p').css('margin-top' , "8em");	
-		$('#section4 .button').css('font-size' , width/1284 * 100 + "%");
-		$('#section4 .button').css('margin-top' , "17em");
-		$('#section4 .button').css('margin-left' , "5em");	
-		
-		$('#rightSmall').css('height', width/1284 * 50 + "em");
 		
 
 
 	});
 
+});
+
+$(window).scroll(function(){
+    $('#scrolling-header').css({
+        'top': $(this).scrollTop(),
+    });
 });
